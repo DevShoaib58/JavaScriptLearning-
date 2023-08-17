73d5c3fb6dc2d3c86842bffcddc29e36837fa51a
Shoaib Qureshi <shoaib@solutionsurface.com>
2023-08-17 13:01:37 +0500

    Practicing JS String Method

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-let cars = ["Honda", "Toyota", "KIA", "Suzuki"]
-document.write("<h1>Simply Showing Data</h1>")
-document.write("<h2>" + cars + "</h2>")
-for (i = 0; i < 4; i++) {
-    document.write("<li>" + cars[i] + "</li>")
+function myfunction() {
+    let txt = "i can eat bananas All Day"
+    console.log(txt.length)
+    console.log(txt.slice(10, 17))
 }
