589775c7cf772aa03eb90cf751cc17a0d1a466c9
Shoaib Qureshi <shoaib@solutionsurface.com>
2023-10-03 16:59:08 +0500

    Practicing New Examples

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function myfunction() {
-    let txt = "i can eat bananas All Day"
-    console.log(txt.length)
-    console.log(txt.slice(10, 17))
+    // let txt = "i can eat bananas All Day"
+    // console.log(txt.length)
+    // console.log(txt.slice(10, 17))
 }
