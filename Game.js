7ece42fe0fc6d141ec4175d6ac749d33aa1cd5f0
Shoaib Qureshi <shoaib@solutionsurface.com>
2023-12-05 15:28:11 +0500

    Try Catch

--- Game.js
+++ Game.js
@@ -1,31 +1,56 @@
-let n1 = Math.floor(Math.random() * 10000)
-let p1 = new Promise((resolve, rejected) => {
-    console.log("Promise is Pending")
-    setTimeout(() => {
-        console.log("I am Promise and i am Fulfilled")
-        resolve(true)
-    }, n1)
-    console.log('n1: ', n1);
-})
-let n2 = Math.floor(Math.random() * 10000)
-let p2 = new Promise((resolve, rejected) => {
-    console.log("Promise is Pending")
-    setTimeout(() => {
-        console.log("I am Promise and i am Rejected")
-        rejected(new Error("i'm Error"))
-    }, n2)
-    console.log('n2: ', n2);
-    // }, Math.floor(Math.random() * 10000) ) For random value in 9000 
-})
-p1.then((value) => {
-    console.log(value);
-})
-p2.catch((error) => {
-    console.log("Throwing Some Error");
-})
-// if any one of them got error then all_promises wont run 
-// let all_promises = Promise.all([p1, p2])
-let all_promises = Promise.allSettled([p1, p2])
-all_promises.then((value) => {
-    console.log("All Promises are resolved", value);
-})
+// let n1 = Math.floor(Math.random() * 10000)
+// let p1 = new Promise((resolve, rejected) => {
+//     console.log("Promise is Pending")
+//     setTimeout(() => {
+//         console.log("I am Promise and i am Fulfilled")
+//         resolve(true)
+//     }, n1)
+//     console.log('n1: ', n1);
+// })
+// let n2 = Math.floor(Math.random() * 10000)
+// let p2 = new Promise((resolve, rejected) => {
+//     console.log("Promise is Pending")
+//     setTimeout(() => {
+//         console.log("I am Promise and i am Rejected")
+//         rejected(new Error("i'm Error"))
+//     }, n2)
+//     console.log('n2: ', n2);
+//     // }, Math.floor(Math.random() * 10000) ) For random value in 9000
+// })
+// p1.then((value) => {
+//     console.log(value);
+// })
+// p2.catch((error) => {
+//     console.log("Throwing Some Error");
+// })
+// // if any one of them got error then all_promises wont run
+// // let all_promises = Promise.all([p1, p2])
+// let all_promises = Promise.allSettled([p1, p2])
+// all_promises.then((value) => {
+//     console.log("All Promises are resolved", value);
+// })
+
+// Try catch Error in JS
+// setTimeout(() => {
+//     console.log("Hello World! 1000")
+// }, 1000)
+// setTimeout(() => {
+//     console.log("Hello World! 2000")
+// }, 2000)
+// try {
+//     // Try to run this code
+//     console.log(Hello);
+// }
+// catch (err) {
+//     // if any error, Code throws the error
+//     console.log(err);
+// }
+// setTimeout(() => {
+//     console.log("Hello World! 3000")
+// }, 3000)
+// setTimeout(() => {
+//     console.log("Hello World! 4000")
+// }, 4000)
+// setTimeout(() => {
+//     console.log("Hello World! 5000")
+// }, 5000)
