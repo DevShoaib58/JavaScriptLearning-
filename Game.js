099e7cf65b035ec9de206a3cad280a6286a3f36f
Shoaib Qureshi <shoaib@solutionsurface.com>
2023-12-08 15:22:01 +0500

    POST API

--- Game.js
+++ Game.js
@@ -101,19 +101,19 @@ const Call_API = async () => {
 //     .then((response) => response.json())
 //     .then((json) => console.log(json));
 
-const post_call = async () => {
-    let p = await fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
-        body: JSON.stringify({
-            title: 'foo',
-            body: 'bar',
-            userId: 1,
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    })
-        .then((response) => response.json())
-        .then((json) => console.log(json));
-}
-post_call();
+// const post_call = async () => {
+//     let p = await fetch('https://jsonplaceholder.typicode.com/posts', {
+//         method: 'POST',
+//         body: JSON.stringify({
+//             title: 'foo',
+//             body: 'bar',
+//             userId: 1,
+//         }),
+//         headers: {
+//             'Content-type': 'application/json; charset=UTF-8',
+//         },
+//     })
+//         .then((response) => response.json())
+//         .then((json) => console.log(json));
+// }
+// post_call();
