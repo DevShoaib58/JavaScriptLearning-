cb744ddcfb5354b78c234acc87efc0c7a4639eae
Shoaib Qureshi <shoaib@solutionsurface.com>
2024-01-01 16:05:28 +0500

    Game Completed

--- Game/GuessNumber.js
+++ Game/GuessNumber.js
@@ -1,31 +1,35 @@
-let RandomNumber = parseInt(Math.random() * 10);
 // console.log(RandomNumber)
-// console.log(GuessedNumber)
-// console.log(typeof (GuessedNumber))
+// console.log(EnteredNumber)
+// console.log(typeof (EnteredNumber))
 //This Will Only tell you the Number is Correct or Not
-// if (GuessedNumber === RandomNumber) {
-//     console.log(`You Have Entered the Right Number your Entered Number is ${GuessedNumber} and Random Number is ${RandomNumber} `)
+// if (EnteredNumber === RandomNumber) {
+//     console.log(`You Have Entered the Right Number your Entered Number is ${EnteredNumber} and Random Number is ${RandomNumber} `)
 // } else {
-//     console.log(`You Have Entered the Wrong Number your Entered Number is ${GuessedNumber} and Random Number is ${RandomNumber} `)
+//     console.log(`You Have Entered the Wrong Number your Entered Number is ${EnteredNumber} and Random Number is ${RandomNumber} `)
 // }
 // }
 const Game = () => {
-    let GuessedNumber = prompt("Enter Number")
-    // while (RandomNumber != GuessedNumber) {
-    GuessedNumber = Number.parseInt(GuessedNumber);
-    if (RandomNumber > GuessedNumber) {
+    let RandomNumber = parseInt(Math.random() * 10);
+    // let attempts = 0;
+    let EnteredNumber = prompt("Enter Number")
+    // while (RandomNumber != EnteredNumber) {
+    EnteredNumber = Number.parseInt(EnteredNumber);
+    if (RandomNumber > EnteredNumber) {
+        // attempts++
         console.log("Entered number is Less then Radom Number");
         console.log("Random Number", RandomNumber);
-        console.log("Guessed Number", GuessedNumber);
+        console.log("Entered Number", EnteredNumber);
         Game();
     }
-    else if (RandomNumber < GuessedNumber) {
+    else if (RandomNumber < EnteredNumber) {
         console.log("Random Number is Greater then Random Number");
         console.log("Entered Number", RandomNumber);
-        console.log("Guessed Number", GuessedNumber);
+        console.log("Entered Number", EnteredNumber);
         Game();
-    } else if (RandomNumber === GuessedNumber) {
-        console.log(`You have Entered the Right Number ${GuessedNumber} and ${RandomNumber}`)
+    } else {
+        console.log(`You have Entered the Right Number ${EnteredNumber} and ${RandomNumber}`)
+        // console.log(attempts)
+
     }
 
 }
