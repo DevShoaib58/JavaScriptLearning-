ef41d675736cf3e975af7152c2aae1ceab2b4c90
Shoaib Qureshi <shoaib@solutionsurface.com>
2024-01-09 17:44:34 +0500

    Array method changes accept from Dev User

--- Practice/ArrayMethod.js
+++ Practice/ArrayMethod.js
@@ -10,16 +10,6 @@
 // })
 // console.log(m)
 /********                       Array Filter Methods in JS                               *********/
-<<<<<<< HEAD
-// let fa = [2, 4, 8, 6, 20, 54, 44, 13, 16, 11, 55, 11, 71];
-// let result = fa.filter((a) => {
-//   a % 2 === 0;
-// });
-// console.log("These are even Numbers ", result);
-// const list = [1, 2, 3, 4];
-// let result1 = list.filter((el) => el % 2 === 0);
-// console.log(result1);
-=======
 // let fa = [2, 4, 8, 6, 20, 54, 44, 13, 16, 11, 55, 11, 71,]
 // let result = fa.filter((a) => {
 //     if (a % 2 == 0) {
@@ -28,4 +18,3 @@
 //         console.log("These are odd numbers", result)
 //     }
 // })
->>>>>>> 13ed9ccc0739e8bcd83ddce257c61fbf60fdabb7
